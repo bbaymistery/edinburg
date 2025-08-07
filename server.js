f8efbbfcd7e3c8399c1eea88490343c5f07fed9d
bbaymistery <elgun.ezmemmedov@mail.ru>
2025-08-07 11:51:26 +0400

    adding compression for next js packgae

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
+const compression = require('compression');
 const dev = false //process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = createServer((req, res) => {
-   
+    compression()(req, res, () => {
       // Be sure to pass `true` as the second argument to `url.parse`.
       // This tells it to parse the query portion of the URL.
       const parsedUrl = parse(req.url, true);
@@ -20,7 +21,7 @@ app.prepare().then(() => {
       } else {
         handle(req, res, parsedUrl);
       }
-    
+    });
   });
 
   server.listen(9000, (err) => {
